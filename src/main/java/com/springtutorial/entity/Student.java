aa560179b6838ec40dbbad1a252d70bb46d57c0a
Murali Krishna <mailstomuralikrishna@gmail.com>
2023-08-16 20:53:28 +0530

    table altered using Annotations

--- src/main/java/com/springtutorial/entity/Student.java
+++ src/main/java/com/springtutorial/entity/Student.java
@@ -1,7 +1,6 @@
 package com.springtutorial.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,12 +11,34 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Table(
+        name="student_tbl",
+        uniqueConstraints = @UniqueConstraint(
+                name="emailid_unique",
+                columnNames = "email_address"
+
+        )
+)
 public class Student {
 
     @Id
+    @SequenceGenerator(
+            name="student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private Long studentId;
     private String firstName;
     private String lastName;
+
+    @Column(
+            name="email_address",
+            nullable = false
+    )
     private String emailId;
     private String guardianName;
     private String guardianEmail;
